1c6a935f0642b45a91535d3e19aa0bf3745b08d1
watotomarketplace <159472388+watotomarketplace@users.noreply.github.com>
2026-02-20 21:55:11 +0300

    Add files via upload

--- server/services/risk.js
+++ server/services/risk.js
@@ -1,4 +1,4 @@
-export async function calculateRiskScore(user, enrollment) {
+export function calculateRiskScore(user, enrollment) {
     const NOW = new Date();
     const MS_PER_DAY = 1000 * 60 * 60 * 24;
 
